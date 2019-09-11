fe3cd03068cfcfe2bfca657a7f2df780fb9c95a1
jyoti ranjan giri <jyotigiri.giri48@gmail.com>
2019-09-11 23:39:38 +0530

    search code lil changed

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # yelp-fusion-search-icecream
+
+#install - npm install
+#run - node sample.js
--- sample.js
+++ sample.js
@@ -1,30 +1,19 @@
-'use strict';
-
 const yelp = require('yelp-fusion');
-
-// Place holder for Yelp Fusion's API Key. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
 const apiKey = 'IxsHkHKxhXv2J9ijKnc3FKoL-HjpdvhmL6819Q2byKAHx47xM5UIIJnH_QL2hU8JYFyFWpE2WQPOGOfKCTfGd9H-Nu0AZLOrRcG0Kk6iIOVLTFu6ZA3Zaqu9peR3XXYx';
-
-const searchRequest = {
-  term: 'ice cream',
-  location: 'Alpharetta, GA'
-};
-
 const client = yelp.client(apiKey);
 let arr = [];
 
 async function yelp_fusion() {
+  const searchRequest = {
+    term: 'ice cream',
+    location: 'Alpharetta, GA'
+  };
   await client.search(searchRequest).then(async response => {
     const firstResult = response.jsonBody.businesses;
-
     for (let i = 0; i < firstResult.length; i++) {
-
       let obj = { "business name": firstResult[i].name, "business address": firstResult[i].location.display_address };
-
       await client.reviews(`${firstResult[i].alias}`).then(async response => {
         let temp_arr = [];
-
         for (let j = 0; j < response.jsonBody.reviews.length; j++) {
           temp_arr.push({ "text": response.jsonBody.reviews[j].text, "name": response.jsonBody.reviews[j].user.name });
         }
@@ -33,13 +22,10 @@ async function yelp_fusion() {
         console.log(error);
       });
       arr.push(obj);
-      // console.log("obj=====================",obj)
-
       if (i == 4) {
         break;
       }
     }
-    // console.log(arr)
     const prettyJson = JSON.stringify(arr, " ", 4);
     console.log(prettyJson);
   }).catch(error => {
